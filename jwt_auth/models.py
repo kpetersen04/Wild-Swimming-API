3f9ed041fa87d4615cc04b08dd74bfbdd18717b0
Kirstin <kpetersen@elon.edu>
2023-05-03 20:25:13 +0100

    remove ImageField related code + update max characters for bio to 600

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -5,8 +5,8 @@ class User(AbstractUser):
     email = models.EmailField()
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
-    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)
-    bio = models.TextField(max_length=400, blank=True)
+    profile_photo = models.URLField( blank=True)
+    bio = models.TextField(max_length=800, blank=True)
     # locations_rated = models.ManyToManyField('swim_sites.Swim_site', blank=True)
     # followers = 
     # following = 
